d899e263ea6b082c441b2cd6b37deaa794da9aa4
boguckiw <boguckiw@student.mini.pw.edu.pl>
2021-03-23 17:54:33 +0100

    Added stochastic gradient descent v1

--- src/gradient_descent.R
+++ src/gradient_descent.R
@@ -3,7 +3,6 @@ jain_split_train
 
 X <- as.matrix(jain_split_train[,-ncol(jain_split_train)])
 
-X <- cbind(rep(1, nrow(X)), X)
 
 Y <- jain_split_train[,ncol(jain_split_train)]
 levels(Y) <- c(0,1)
@@ -44,6 +43,30 @@ gradient_descent <- function(X,Y, n_iter, lr){
   return(round(sigmoid(X %*% theta)))
 }
 
+stoch_gradient_descent <- function(X,Y, n_iter, lr){
+  m <- nrow(X)
+  X <- cbind(rep(1, m), X)
+  n <- ncol(X)
+  theta <- init_theta(n)
+  for(i in 1:n_iter){
+    idx <- sample(1:m, m)
+    x_new <- X[idx,]
+    y_new <- Y[idx]
+    cost <- 0
+    for(i in 1:m){
+      x <- x_new[i,]
+      y <- y_new[i]
+      g <- sigmoid(x %*% theta)
+      cost <- cost - (y*log(g + 1e-8) + (1-y)*log(1-g + 1e-8))
+      theta <- theta - lr*(x * c(g - y))
+    }
+    print(paste0("Cost: ", round(cost/m,4)))
+  }
+  return(round(sigmoid(X %*% theta)))
+}
+
 y_hat <- gradient_descent(X, Y, 1000, 0.02)
+y_hat2 <- stoch_gradient_descent(X, Y, 100, 0.005)
 source("src/measure.R")
 measure(y_hat, Y)
+measure(y_hat2, Y)
