e825ad6d3b4292e8617a7452f66240364202e32b
Pastuszka <michal.pastuszka98@gmail.com>
2021-03-24 16:24:44 +0100

    different stop condition

--- src/iwls.R
+++ src/iwls.R
@@ -1,30 +1,33 @@
+require(Matrix)
 
-
-iwls <- function(X, y, epsilon = 1e-4, max_iter = 100){
-  n <- 1:nrow(X)
+iwls <- function(X, y, epsilon = 1e-4, max_iter = 1000){
+  
+  m <- nrow(X)
+  m_vec <- 1:m
   X <- cbind(1, X)
-  y0 <- mean(y)
   beta <- as.matrix(rep(0, length.out = ncol(X)))
-  beta[1] <- log(y0/(1-y0))
-  n_iter <- 1
+  p <- 1 / (1 + exp(- X %*% beta))
+  all_costs <- c(-sum(y*log(p + 1e-8) + (1-y)*log(1-p + 1e-8))/m)
   
   for(i in 1:max_iter){
-    
-    beta_old <- beta
-    p <- 1 / (1 + exp(- X %*% beta_old))
-    W <- Matrix::sparseMatrix(n, n, x = as.numeric(p * (1 - p)))
-    W_inv <- Matrix::sparseMatrix(n, n, x = as.numeric(1 / (p * (1 - p))))
-    z <- X%*%beta_old + W_inv %*% (y - p)
+    p_vec <- as.numeric(p * (1 - p)) + 1e-8
+    W <- sparseMatrix(m_vec, m_vec, x = p_vec)
+    W_inv <- sparseMatrix(m_vec, m_vec, x = p_vec)
+    z <- X%*%beta + W_inv %*% (y - p)
     beta <- solve(t(X) %*% W %*% X) %*% t(X) %*% W %*% z
+    p <- 1 / (1 + exp(- X %*% beta))
     
-    if(all(abs(beta - beta_old) < epsilon)) break
+    all_costs[i+1] <- -sum(y*log(p + 1e-8) + (1-y)*log(1-p + 1e-8))/m
+
+    if(all(abs(all_costs[i+1] - all_costs[i]) < epsilon)) break
     
     if(i == max_iter){
       warning(paste('Algorithm did not converge after max_iter =', max_iter))
       
     }
   }
-  structure(.Data  = list(beta = beta, iters = i), class = c("iwls", "logreg", "model"))
+  structure(.Data  = list(beta = beta, iters = i, costs = all_costs), 
+            class = c("iwls", "logreg", "model"))
 }
 
 predict.iwls <- function(object, X, prob = FALSE, ...){
